f83ad7e5aba77e7bca878b06d6bcdf34eea7cd81
Денис <garaevdenis2019@gmail.com>
2023-05-19 21:31:40 +0500

    HW is done

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ import java.util.*;
 
 public class Main {
     private static final List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
-    private static final List<String> words = new ArrayList<>(List.of("один", "два","два", "три", "три", "три"));
+    private static final List<String> words = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
+
     public static void main(String[] args) {
         task1();
         space();
@@ -18,7 +19,8 @@ public class Main {
     public static void space() {
         System.out.print(" ");
     }
-//    Напишите код, с помощью которого можно напечатать только нечетные числа в консоль.
+
+    //    Напишите код, с помощью которого можно напечатать только нечетные числа в консоль.
 //    Код должен работать с любой последовательностью и объемом списка чисел.
     public static void task1() {
         for (int num : nums) {
@@ -27,10 +29,11 @@ public class Main {
             }
         }
     }
+
     public static void task2() {
 //        Напишите код, с помощью которого можно напечатать только четные числа без повторений в порядке возрастания.
         Collections.sort(nums);
-        int prevNum= Integer.MIN_VALUE;
+        int prevNum = Integer.MIN_VALUE;
         for (int num : nums) {
             if (num % 2 == 0 && num != prevNum) {
                 prevNum = num;
@@ -38,11 +41,13 @@ public class Main {
             }
         }
     }
+
     public static void task3() {
         Set<String> uniqueWords = new HashSet<>(words);
         System.out.print(uniqueWords);
 //        Напишите код, который выводит в консоль все уникальные слова из списка слов, в котором могут встречаться дубли.
     }
+
     public static void task4() {
         Set<String> uniqueWords = new HashSet<>(words);
         System.out.print(words.size() - uniqueWords.size());
